0135edd00a84ec0edd2f1752ea60b3c2fbb7a79b
Atrusilo1 <atrusilo1@studentmail.nait.ca>
2016-10-18 16:01:19 -0600

    Made the UnRegistered Profile

--- ChinookSample/ChinookSystem/Security/UnRegisteredUserProfile.cs
+++ ChinookSample/ChinookSystem/Security/UnRegisteredUserProfile.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.Security
 {
-    class UnRegisteredUserProfile
+    public enum UnRegisteredUserType { Underfined, Employee, Customer }
+    public class UnRegisteredUserProfile
     {
+        public string UserId { get; set; } //generated 
+        public string UserName { get; set; } //collected
+        public string EmailAddress { get; set; } //collected 
+        public string FirstName { get; set; } //Comes from User Table
+        public string LastName { get; set; } //Comes from the User Table
+        public UnRegisteredUserType UserType { get; set; }
+
     }
 }
