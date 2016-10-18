7ff92a4f9fdf81e819864d1c9d37f05f97bc11e5
Atrusilo1 <atrusilo1@studentmail.nait.ca>
2016-10-18 16:07:48 -0600

    Created UserProfiles class

--- ChinookSample/ChinookSystem/Security/UserProfile.cs
+++ ChinookSample/ChinookSystem/Security/UserProfile.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.Security
 {
-    class UserProfile
+    public class UserProfile
     {
+        public string UserId { get; set; }  //AspNetUser Table
+        public string UserName { get; set; }    //AspNetUserTable 
+        public int? EmployeeId { get; set; }    //AspNetUserTable
+        public int? CustomerId { get; set; }    //AspNetUserTable
+        public string FirstName { get; set; }   //Employee or Customer Tables
+        public string LastName { get; set; }    //Employee or customer tables
+        public string Email { get; set; }   //AspNetUser Table
+        public string EmailConfirmed { get; set; }  //AspNetUser Table
+        public IEnumerable<string> RoleMemerships { get; set; }
     }
 }
